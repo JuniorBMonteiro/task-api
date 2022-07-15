26c7294fead969c058be538412eadc9dc75c6e89
JuniorMonteiro <jmonteiro@vertigo.com.br>
2022-07-15 11:51:18 -0300

    Inicio do projeto

---
+++ src/test/java/br/com/bmont/task/TaskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bmont.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
